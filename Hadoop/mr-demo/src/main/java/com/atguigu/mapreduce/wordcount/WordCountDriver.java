69dba4f924c16a04b3b3ead313add01651404b13
ftruezzq <ftruezzq@163.com>
2021-08-15 19:32:16 +0800

    map输出端压缩  reduce端压缩

--- Hadoop/mr-demo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountDriver.java
+++ Hadoop/mr-demo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountDriver.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -19,6 +21,19 @@ public class WordCountDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         //获取配置信息以及获取job对象
         Configuration conf = new Configuration();
+
+        //开启map端输出压缩的方式1   推荐
+        conf.set("mapreduce.map.output.compress", "true");
+        conf.set("mapreduce.map.output.compress.codec", "org.apache.hadoop.io.compress.DefaultCodec");
+
+        //开启map端输出压缩的方式2
+        /*
+        // 开启map端输出压缩
+        conf.setBoolean("mapreduce.map.output.compress", true);
+        // 设置map端输出压缩方式
+        conf.setClass("mapreduce.map.output.compress.codec", BZip2Codec.class, CompressionCodec.class);
+        */
+
         Job job = Job.getInstance(conf);
         //关联本Driver程序的jar
         job.setJarByClass(WordCountDriver.class);
@@ -31,9 +46,22 @@ public class WordCountDriver {
         //设置最终输出的kv类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+        //设定一下reduce的个数
+        job.setNumReduceTasks(1);
+
+        //设置使用combiner
+        //job.setInputFormatClass(CombineTextInputFormat.class);
+        //CombineTextInputFormat.setMaxInputSplitSize(job, 20971520);
+
+        // 设置reduce端输出压缩开启
+        FileOutputFormat.setCompressOutput(job, true);
+        // 设置压缩的方式
+        FileOutputFormat.setOutputCompressorClass(job, BZip2Codec.class);
+
         //设置输入和输出路径
-        FileInputFormat.setInputPaths(job, new Path("E:\\input\\inputFlow"));  //new Path(args[0])
-        FileOutputFormat.setOutputPath(job, new Path("E:\\output\\outflow2"));  //new Path(args[1])
+        FileInputFormat.setInputPaths(job, new Path("E:\\input\\inputbigwords"));
+        FileOutputFormat.setOutputPath(job, new Path("E:\\hadoop\\compress5"));
         //提交job
         boolean result = job.waitForCompletion(true);
         System.exit(result ? 0 : 1);
